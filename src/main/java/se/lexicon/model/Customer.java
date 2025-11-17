3ae794322308c6c4c2fd99bd26931b80f0318ca8
Ashbinder666 <ashxela@gmail.com>
2025-11-17 15:42:28 +0100

    Initial design of the file, with the constructor, getters and setters. Also fixed a template typo of "Plat" into "Plate".

--- src/main/java/se/lexicon/model/Customer.java
+++ src/main/java/se/lexicon/model/Customer.java
@@ -1,5 +1,7 @@
 package se.lexicon.model;
 
+import se.lexicon.dao.sequencer.CustomerIdSequencer;
+
 /**
  * Represents a customer who uses the parking reservation system.
  * <p>
@@ -15,11 +17,59 @@ public class Customer {
     private Integer id;
     private String name;
     private String phoneNumber;
-    private String vehiclePlatNumber;
+    private String vehiclePlateNumber;
 
     // TODO: Add constructors
 
+    public Customer(String name, String phoneNumber, String vehiclePlateNumber) {
+        this.id = CustomerIdSequencer.nextId();
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.vehiclePlateNumber = vehiclePlateNumber;
+    }
+
+
     // TODO: Add getters and setters
 
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getVehiclePlateNumber() {
+        return vehiclePlateNumber;
+    }
+
+    public void setVehiclePlateNumber(String vehiclePlateNumber) {
+        this.vehiclePlateNumber = vehiclePlateNumber;
+    }
+
+
     // TODO: Add toString() method if needed
+
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", vehiclePlateNumber='" + vehiclePlateNumber + '\'' +
+                '}';
+    }
 }

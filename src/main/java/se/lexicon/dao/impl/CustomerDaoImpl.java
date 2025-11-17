8ea57f92ba8ee6db445d0ce66a17c78ae0d0250a
Ashbinder666 <ashxela@gmail.com>
2025-11-17 16:27:18 +0100

    Added implements, methods and customer list and overrides.

--- src/main/java/se/lexicon/dao/impl/CustomerDaoImpl.java
+++ src/main/java/se/lexicon/dao/impl/CustomerDaoImpl.java
@@ -1,7 +1,31 @@
 package se.lexicon.dao.impl;
 
 
-public class CustomerDaoImpl {
+import se.lexicon.dao.CustomerDao;
+import se.lexicon.model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerDaoImpl implements CustomerDao {
+
+    private List<Customer> customers = new ArrayList<>();
 
 
+    @Override
+    public Customer create(Customer customer) {
+        customers.add(customer);
+        return customer;
+    }
+
+    @Override
+    public Optional<Customer> findById(int id) {
+        for (Customer customer : customers) {
+            if (customer.getId() == id) {
+                return Optional.of(customer);
+            }
+        }
+        return Optional.empty();
+    }
 }

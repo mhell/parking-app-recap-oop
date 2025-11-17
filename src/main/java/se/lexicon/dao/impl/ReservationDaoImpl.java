0bada17628dff124f1b7b649ec1bb52989794264
ZicoCoder <Azzoug.zackaria@gmail.com>
2025-11-17 20:36:37 +0100

    add initial ReservationDaoImpl skeleton

--- src/main/java/se/lexicon/dao/impl/ReservationDaoImpl.java
+++ src/main/java/se/lexicon/dao/impl/ReservationDaoImpl.java
@@ -1,5 +1,31 @@
 package se.lexicon.dao.impl;
+import se.lexicon.dao.ReservationDao;
+import se.lexicon.model.Reservation;
 
-public class ReservationDaoImpl {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ReservationDaoImpl implements ReservationDao {
+
+    private final List<Reservation> reservations = new ArrayList<>();
+
+    @Override
+    public Optional<Reservation> findById(String reservationID) {
+        return reservations.stream()
+                .filter(r -> r.getReservationId().equals(reservationID))
+                .findFirst();
+    }
+
+    @Override
+    public void update(Reservation reservation) {
+        for (int i = 0; i < reservations.size(); i++) {
+            if (reservations.get(i).getReservationId().equals(reservation.getReservationId())) {
+                reservations.set(i, reservation);
+                return;
+            }
+        }
+        throw new IllegalArgumentException("Reservation not found: " + reservation.getReservationId());
+    }
 
 }

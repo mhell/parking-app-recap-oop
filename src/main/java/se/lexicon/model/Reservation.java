1edfcef8c131d60456e05beb4e66556c3d395c8f
Iffat-Sudo <zabin.ifa@gmail.com>
2025-11-18 11:14:46 +0100

    Added all the changes

--- src/main/java/se/lexicon/model/Reservation.java
+++ src/main/java/se/lexicon/model/Reservation.java
@@ -1,18 +1,9 @@
 package se.lexicon.model;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
-/**
- * Represents a reservation made by a customer for a specific parking spot.
- * <p>
- * A reservation records which customer reserved which parking spot,
- * along with the start and end time of the reservation and its current status.
- */
 public class Reservation {
-
-    public String getReservationId() {
-        return reservationId;
-    } // Needed by ReservationDaoImpl to look up reservations by id
 
     private String reservationId;
     private LocalDateTime startTime;
@@ -21,26 +12,86 @@ public class Reservation {
     private ParkingSpot parkingSpot;
     private Customer customer;
 
-    // TODO: Add constructors
 
-    // TODO: Add getters and setters
+    public Reservation(String reservationId, LocalDateTime startTime, LocalDateTime endTime, Status status, ParkingSpot parkingSpot, Customer customer) {
+        this.reservationId = reservationId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.status = status;
+        this.parkingSpot = parkingSpot;
+        this.customer = customer;
+    }
+
+
+    public void setReservationId(String reservationId) {
+        this.reservationId = UUID.randomUUID().toString();
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public ParkingSpot getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(ParkingSpot parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
 
     // TODO: Add toString() if needed
 
-    /**
-     * Marks the reservation as completed.
-     * Changes status from ACTIVE to COMPLETED.
-     */
-    public void complete() {
-        // TODO: Implement by setting status to COMPLETED
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "reservationId='" + reservationId + '\'' +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", status=" + status +
+                ", parkingSpot=" + parkingSpot +
+                ", customer=" + customer +
+                '}';
     }
 
-    /**
-     * Sets the end time of the reservation based on a duration.
-     *
-     * @param hours Number of hours for the reservation
-     */
-    public void setEndTimeByHours(int hours) {
-        // TODO: Implement by adding hours to startTime
+    public void complete() {
+        this.status = Status.COMPLETED;
     }
+
+
+    public void setEndTimeByHours(int hours) {
+        if (startTime != null) {
+            this.endTime = startTime.plusHours(hours);
+        }
+    }
+
 }

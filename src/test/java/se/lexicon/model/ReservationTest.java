b90c00b260e7ff235fb0b9fba968059bca5a7566
Iffat-Sudo <zabin.ifa@gmail.com>
2025-11-18 15:39:38 +0100

    Added all the changes

---
+++ src/test/java/se/lexicon/model/ReservationTest.java
@@ -0,0 +1,29 @@
+package se.lexicon.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getEndTime() {
+    }
+
+    @Test
+    void setEndTime() {
+    }
+
+    @Test
+    void complete() {
+    }
+
+    @Test
+    void setEndTimeByHours() {
+    }
+}

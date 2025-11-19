d84ac36ef51d81f2a65ecc0eaa675e4aaa8da17f
Sindhuja Parthasarathy <sarathy.sindhu@gmail.com>
2025-11-19 16:47:44 +0100

    Completed the GUI for parkingApp

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -12,21 +12,25 @@ import se.lexicon.service.ParkingService;
 import se.lexicon.service.ReservationService;
 import se.lexicon.service.VacateService;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        CustomerDao customerDao = new CustomerDaoImpl();
-        ParkingSpotDao parkingSpotDao = new ParkingSpotDaoImpl();
-        ReservationDao reservationDao = new ReservationDaoImpl();
+//        CustomerDao customerDao = new CustomerDaoImpl();
+//        ParkingSpotDao parkingSpotDao = new ParkingSpotDaoImpl();
+//        ReservationDao reservationDao = new ReservationDaoImpl();
+//
+//        parkingSpotDao.create(new ParkingSpot(1,101,false));
+//        parkingSpotDao.create(new ParkingSpot(2,102,false));
+//        parkingSpotDao.create(new ParkingSpot(3,103,false));
+//
+//        CustomerService costumerService = new CustomerService(customerDao);
+//        ParkingService parkingService = new ParkingService(parkingSpotDao);
+//        ReservationService reservationService = new ReservationService(reservationDao, parkingSpotDao);
+//        VacateService vacateService = new VacateService(reservationDao);
+//
+//        new ParkingApp(costumerService, parkingService, reservationService, vacateService).start();
 
-        parkingSpotDao.create(new ParkingSpot(1,101,false));
-        parkingSpotDao.create(new ParkingSpot(2,102,false));
-        parkingSpotDao.create(new ParkingSpot(3,103,false));
-
-        CustomerService costumerService = new CustomerService(customerDao);
-        ParkingService parkingService = new ParkingService(parkingSpotDao);
-        ReservationService reservationService = new ReservationService(reservationDao, parkingSpotDao);
-        VacateService vacateService = new VacateService(reservationDao);
-
-        new ParkingApp(costumerService, parkingService, reservationService, vacateService).start();
+        SwingUtilities.invokeLater(ParkingAppUI::new);
     }
 }
